08ea5aab0c5248e942c176860a5fef05869b04a3
tfoxy <tomas.c.fox@gmail.com>
2015-06-30 18:59:53 -0300

    Add font size test

--- src/test/java/tf/resizablelabel/ResizableLabelTest.java
+++ src/test/java/tf/resizablelabel/ResizableLabelTest.java
@@ -1,6 +1,10 @@
 package tf.resizablelabel;
 
+import org.junit.Assert;
 import org.junit.Before;
+import org.junit.Test;
+
+import javax.swing.*;
 
 public class ResizableLabelTest {
     private ResizableLabel label;
@@ -8,5 +12,14 @@ public class ResizableLabelTest {
     @Before
     public void initialize() {
         label = new ResizableLabel();
+    }
+
+    @Test
+    public void hasDefaultFontSizeAsJLabelWhenCreated() {
+        JLabel jLabel = new JLabel();
+        int jLabelSize = jLabel.getFont().getSize();
+        int size = label.getFont().getSize();
+
+        Assert.assertEquals(jLabelSize, size);
     }
 }
