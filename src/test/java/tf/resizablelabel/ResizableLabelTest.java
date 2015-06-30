0da25666c3f803a7fea8f84736650dac9368aca1
tfoxy <tomas.c.fox@gmail.com>
2015-06-30 18:46:23 -0300

    Add ResizableLabelTest.java

---
+++ src/test/java/tf/resizablelabel/ResizableLabelTest.java
@@ -0,0 +1,12 @@
+package tf.resizablelabel;
+
+import org.junit.Before;
+
+public class ResizableLabelTest {
+    private ResizableLabel label;
+
+    @Before
+    public void initialize() {
+        label = new ResizableLabel();
+    }
+}
